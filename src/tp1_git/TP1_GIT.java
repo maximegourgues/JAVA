fce0021dbc0d80e28a92818f829f489fe82b33b7
mgourg01 <mgourg01@pcalb-mm0611.univ-jfc.fr>
2019-09-17 13:42:25 +0200

    mon premier tp git

---
+++ src/tp1_git/TP1_GIT.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1_git;
+
+/**
+ *
+ * @author mgourg01
+ */
+public class TP1_GIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+/** Salu c cool*/
